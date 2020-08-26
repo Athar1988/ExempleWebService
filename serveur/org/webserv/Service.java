92968fe2976215c1aa426cf1b322b6b99f1967be
Athar1988 <hakim.athar.88@gmail.com>
2020-08-26 15:05:16 +0100

    version fini mais avec une erreur

--- serveur/org/webserv/Service.java
+++ serveur/org/webserv/Service.java
@@ -1,12 +1,39 @@
 package org.webserv;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 @Path("/banque")
 public class Service {
 
 	@GET
-	@Path("/simple")
-	public String Simple() {
-		return  "Hello";
+	@Path("/test")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String test(){
+		return "hello test";
 	}
+	
+	@GET
+	@Path("/conversion/{montant}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public double conversion(@PathParam("montant") double mt){
+		return 10*mt;
+	}
+	
+	@GET
+	@Path("/infos")
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<String> getInfos(){
+		List<String> liste = new ArrayList<>();
+		liste.add("A");
+		liste.add("B");
+		return liste;
+	}
+	
 }
